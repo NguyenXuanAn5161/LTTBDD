43bfadafbee56e994b922c693a1b19930e2662b5
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2023-10-05 20:52:06 +0700

    XMEye

--- Lab3/demo/App.js
+++ Lab3/demo/App.js
@@ -3,7 +3,9 @@
 // import FirstScreen from "./pages/FirstScreen";
 // import ManHinh_1_d from "./pages/1_d";
 // import ManHinh_1_c from "./pages/1_c";
-import ManHinh_1_e from "./pages/1_e";
+// import ManHinh_1_e from "./pages/1_e";
+// import ManHinh_2_a from "./pages/2_a";
+import ManHinh_XMEye from "./pages/XMEye";
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
     // <ManHinh_1_b />
     // <ManHinh_1_c />
     // <ManHinh_1_d />
-    <ManHinh_1_e />
+    // <ManHinh_1_e />
+    // <ManHinh_2_a />
+    <ManHinh_XMEye />
   );
 }
