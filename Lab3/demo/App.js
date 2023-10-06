08ffd564a3c63e57d26be91167c3fcd8ee2dac1f
an <an@email.com>
2023-10-06 11:19:04 +0700

    useState

--- Lab3/demo/App.js
+++ Lab3/demo/App.js
@@ -4,18 +4,18 @@
 // import ManHinh_1_d from "./pages/1_d";
 // import ManHinh_1_c from "./pages/1_c";
 // import ManHinh_1_e from "./pages/1_e";
-// import ManHinh_2_a from "./pages/2_a";
-import ManHinh_XMEye from "./pages/XMEye";
+import ManHinh_2_a from "./pages/2_a";
+// import ManHinh_XMEye from "./pages/XMEye";
 
 export default function App() {
-  return (
-    // <FirstScreen />
-    // <ManHinh_1_a />
-    // <ManHinh_1_b />
-    // <ManHinh_1_c />
-    // <ManHinh_1_d />
-    // <ManHinh_1_e />
-    // <ManHinh_2_a />
-    <ManHinh_XMEye />
-  );
+   return (
+      // <FirstScreen />
+      // <ManHinh_1_a />
+      // <ManHinh_1_b />
+      // <ManHinh_1_c />
+      // <ManHinh_1_d />
+      // <ManHinh_1_e />
+      <ManHinh_2_a />
+      // <ManHinh_XMEye />
+   );
 }
